368ba212cb07cef2700032fd13bac4a4bac3e7cb
akashktesla <akashktesla@gmail.com>
2022-08-11 09:12:23 +0000

    :)

--- app/src/main/java/com/example/gother/MainActivity.java
+++ app/src/main/java/com/example/gother/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.gother;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,45 +49,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this,"onstart",Toast.LENGTH_LONG).show();
-        Log.d("_log","onstart");
-    }
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Button btn4;
+        btn4 = findViewById(R.id.btn4);
+        outState.putInt("counter",Integer.valueOf(btn4.getText().toString()));
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this,"onpause",Toast.LENGTH_LONG).show();
-        Log.d("_log","onpause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this,"onresume",Toast.LENGTH_LONG).show();
-        Log.d("_log","onresume");
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this,"onstop",Toast.LENGTH_LONG).show();
-        Log.d("_log","onstop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this,"onrestart",Toast.LENGTH_LONG).show();
-        Log.d("_log","onrestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this,"ondestroy",Toast.LENGTH_LONG).show();
-        Log.d("_log","onDestroy");
     }
 }
